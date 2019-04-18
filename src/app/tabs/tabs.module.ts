6352baf03015518c8b513f6737e179a8721ca9ec
Hayden Miedema <haydenmiedema@gmail.com>
2019-04-17 22:19:12 -0400

    add all initial files

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -2,7 +2,14 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { HomePage } from '../home/home.page';
+import { AddPage } from '../add/add.page';
+import { EditPage } from '../edit/edit.page';
+import { DetailsPage } from '../details/details.page';
+import { AddPageModule } from '../add/add.module';
+import { EditPageModule } from '../edit/edit.module';
+import { HomePageModule } from '../home/home.module';
+import { DetailsPageModule } from '../details/details.module';
 import { TabsPageRoutingModule } from './tabs.router.module';
 
 import { TabsPage } from './tabs.page';
@@ -12,7 +19,11 @@ import { TabsPage } from './tabs.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    HomePageModule,
+    AddPageModule,
+    EditPageModule,
+    DetailsPageModule
   ],
   declarations: [TabsPage]
 })
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -1,52 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
-
 const routes: Routes = [
   {
     path: 'tabs',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab1/tab1.module#Tab1PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab2/tab2.module#Tab2PageModule'
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: '../tab3/tab3.module#Tab3PageModule'
-          }
-        ]
-      },
-      {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
+        redirectTo: '/tabs/(home:home)',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        outlet: 'home',
+        component: HomePage
+      },
+      {
+        path: 'add',
+        outlet: 'add',
+        component: AddPage
+      },
+      {
+        path: ':id',
+        outlet: 'edit',
+        component: EditPage
+      },
+      {
+        path: ':id',
+        outlet: 'details',
+        component: DetailsPage
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/(home:home)',
     pathMatch: 'full'
   }
-];
 
 @NgModule({
   imports: [
